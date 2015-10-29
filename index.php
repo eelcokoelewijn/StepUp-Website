be93d9a13642c7fed3054a22b4411bf5eac2049c
Eelco Koelewijn <eelcokoelewijn@Eelcos-MacBook-Pro.local>
2015-10-29 21:09:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <title>Step Up</title>
+  <style>
+  body{
+  background-color:#5cbb47;
+  color:#fff;
+  font:1.2em/1.2em arial,serief;
+  }
+  a{
+  text-decoration:none;
+  color:#fff;
+  }
+  </style>
+</head>
+<body>
+<div>
+<h2>Step Up!</h2>
+<p>Stepped care bij lichte depressie</p>
+<p><a href="mailto:stepuppsf@gmail.com">stepuppsf@gmail.com</a></p>
+</div>
+</body>
+</html>
